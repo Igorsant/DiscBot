fd315b133e9a54495083e1f8141c53a777dd2fff
Player Ghost <sousaitalo12@gmail.com>
2021-10-20 19:13:18 -0300

    .

--- lib/discbot/consumer.ex
+++ lib/discbot/consumer.ex
@@ -1,11 +1,22 @@
 defmodule Discbot.Consumer do
     use Nostrum.Consumer
+    alias Nostrum.Api
 
     def start_link do
         Consumer.start_link(__MODULE__)
     end
 
-    def handle_event(_) do 
+    def handle_event({:MESSAGE_CREATE, msg, _ws_state}) do
+        cond do
+            msg.content == "!ping" -> Api.create_message(msg.channel_id, "pong")
+            msg.content == "!pong" -> Api.create_message(msg.channel_id, "ping")
+            msg.content == "!react" -> Api.create_reaction!(msg.channel_id, msg.id, "😏")
+            #String.starts_with?(msg.content, "!react with ") -> Api.create_reaction!(msg.channel_id, msg.id, elem(String.split(msg.content, " "), 2))
+            true -> :ok
+        end
+    end
+
+    def handle_event(_) do
         :ok
     end
 end
